3c2a1dbddc4c97f73239df0c7e9c54b8a25cb3ed
Ilya Zlobintsev <ilya.zl@protonmail.com>
2021-03-08 12:43:34 +0200

    Add icon

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,8 @@ sudo install -Dm755 target/release/cli /usr/local/bin/lact-cli &&
 sudo install -Dm644 lactd.service /etc/systemd/system/lactd.service && 
 sudo mkdir -p /usr/local/share/applications && 
 sudo install -Dm644 lact.desktop /usr/local/share/applications/ && 
+sudo mkdir -p /usr/share/icons/hicolor/192x192/apps &&
+sudo install -Dm644 icons/lact.png /usr/share/icons/hicolor/192x192/apps/ &&
 sudo systemctl daemon-reload 
 sudo systemctl enable lactd &&
 sudo systemctl restart lactd
--- uninstall.sh
+++ uninstall.sh
@@ -3,4 +3,5 @@ sudo systemctl disable --now lactd &&
 sudo rm /usr/local/bin/lact-daemon &&
 sudo rm /usr/local/bin/lact-gui &&
 sudo rm /etc/systemd/system/lactd.service &&
-sudo rm /usr/local/share/applications/lact.desktop
+sudo rm /usr/local/share/applications/lact.desktop &&
+sudo rm /usr/share/icons/hicolor/192x192/apps/lact.png
